695b814a710b3e79efc70435b5bfff48907efbbf
Nathan Nunes <nunesxd7@hotmail.com>
2021-12-24 02:39:47 -0300

    :rocket: Newbie fixes #5

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import './styles/bootstrap.css';
 import './styles/myStyles.css';
 import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
   document.getElementById('root'),
 );
