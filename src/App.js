e1941ca9ffa91a70f1d2508eace735c86ab0b0e1
NunesNathan <nunesxd7@hotmail.com>
2022-03-02 01:25:35 -0300

    :recycle: (refactor) - Rename generic routes function

--- src/App.js
+++ src/App.js
@@ -1,41 +1,15 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
-import Footer from './components/Footer';
-import Header from './components/Header';
-import Home from './pages/Home';
-import TaskDetails from './pages/TaskDetails';
+import aListarRouter from './helpers/services';
 import './styles/bootstrap.css';
 import './styles/myStyles.css';
 
-const App = () => {
-  const routes = () => (
-    <Switch>
-      <Route
-        path="/"
-        exact
-      >
-        <Header />
-        <Home />
-        <Footer />
-      </Route>
-      <Route
-        path="/task_details/:taskId"
-      >
-        <Header />
-        <TaskDetails />
-        <Footer />
-      </Route>
-    </Switch>
-  );
-
-  return (
-    <div
-      className="d-flex flex-wrap
-      justify-content-center"
-    >
-      { routes() }
-    </div>
-  );
-};
+const App = () => (
+  <div
+    className="d-flex flex-wrap
+    justify-content-center"
+  >
+    { aListarRouter() }
+  </div>
+);
 
 export default App;
