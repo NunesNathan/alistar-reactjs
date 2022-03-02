f2db3bd8086faea13668ad774eae1580a178fe28
NunesNathan <nunesxd7@hotmail.com>
2022-03-02 01:04:49 -0300

    :art: (refactor) - App is now a function component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Footer from './components/Footer';
 import Header from './components/Header';
@@ -7,37 +7,35 @@ import TaskDetails from './pages/TaskDetails';
 import './styles/bootstrap.css';
 import './styles/myStyles.css';
 
-export default class App extends Component {
-  routes() {
-    return (
-      <Switch>
-        <Route
-          path="/"
-          exact
-        >
-          <Header />
-          <Home />
-          <Footer />
-        </Route>
-        <Route
-          path="/task_details/:taskId"
-        >
-          <Header />
-          <TaskDetails />
-          <Footer />
-        </Route>
-      </Switch>
-    );
-  }
-
-  render() {
-    return (
-      <div
-        className="d-flex flex-wrap
-        justify-content-center"
+const App = () => {
+  const routes = () => (
+    <Switch>
+      <Route
+        path="/"
+        exact
       >
-        { this.routes() }
-      </div>
-    );
-  }
-}
+        <Header />
+        <Home />
+        <Footer />
+      </Route>
+      <Route
+        path="/task_details/:taskId"
+      >
+        <Header />
+        <TaskDetails />
+        <Footer />
+      </Route>
+    </Switch>
+  );
+
+  return (
+    <div
+      className="d-flex flex-wrap
+      justify-content-center"
+    >
+      { routes() }
+    </div>
+  );
+};
+
+export default App;
