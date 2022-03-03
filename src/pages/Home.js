817e6a5ced351cedbf39abbe09a9e3a2d2f30848
NunesNathan <nunesxd7@hotmail.com>
2022-03-03 10:33:47 -0300

    :recycle: (refactor) - Functional Home component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,85 +1,59 @@
-import React, { Component } from 'react';
-import TaskList from '../components/TaskList';
+import React, { useState } from 'react';
 import ListManagement from '../components/ListManagement';
+import TaskList from '../components/TaskList';
 import { createKey, newDate } from '../helpers/easier';
 import * as store from '../helpers/store';
 
-export default class Home extends Component {
-  constructor() {
-    super();
+const STRING_TYPE = '';
 
-    this.state = {
-      inputValue: '',
-      showDescription: false,
-      tasks: store.getTasks(),
-    };
-  }
+export default function home() {
+  const [taskText, setTaskText] = useState(STRING_TYPE);
+  const [tasks] = useState(store.getTasks());
+  const [showDescription, setShowDescription] = useState(false);
 
-  sendTask = () => {
-    const { inputValue } = this.state;
-    if (inputValue) {
-      store.sendTasks({
-        task: inputValue,
-        uniqKey: createKey(inputValue),
-        desc: '',
-        createdOn: newDate(),
-        deadline: '',
-      });
-      this.setState({
-        inputValue: '',
-      });
-    }
-  }
-
-  getTaskName = ({ target: { value } }) => {
-    this.setState({
-      inputValue: value,
-    });
-  }
-
-  switchDescription = () => {
-    const { showDescription: oldValue } = this.state;
-    this.setState({
-      showDescription: !oldValue,
-    });
-  }
-
-  render() {
-    const { inputValue, showDescription, tasks } = this.state;
-    return (
-      <main className="d-flex flex-column container align-items-center">
-        <p className="text-center display-5">Afazeres:</p>
-        <label
-          className="flex-wrap d-flex input-group
+  return (
+    <main className="d-flex flex-column container align-items-center">
+      <p className="text-center display-5">Afazeres:</p>
+      <label
+        className="flex-wrap d-flex input-group
           w-50 row"
-          htmlFor="taskInput"
-        >
-          <input
-            className="form-control col-9 bg-grey-2"
-            placeholder="Tarefa..."
-            value={ inputValue }
-            onChange={ this.getTaskName }
-            id="taskInput"
-            name="taskInput"
-          />
-          <button
-            className="btn-n-outline-success col-3"
-            type="button"
-            onClick={ this.sendTask }
-          >
-            Listar!
-          </button>
-        </label>
-        <ListManagement
-          showDescription={ showDescription }
-          switchDescription={ this.switchDescription }
+        htmlFor="taskInput"
+      >
+        <input
+          className="form-control col-9 bg-grey-2"
+          placeholder="Tarefa..."
+          value={ taskText }
+          onChange={ ({ target: { value } }) => setTaskText(value) }
+          id="taskInput"
+          name="taskInput"
         />
-        {tasks
-          && <TaskList
-            tasks={ tasks }
-            showDescription={ showDescription }
-          />}
-      </main>
-    );
-  }
+        <button
+          className="btn-n-outline-success col-3"
+          type="button"
+          onClick={ () => (
+            taskText
+              && (store.sendTasks({
+                task: taskText,
+                uniqKey: createKey(taskText),
+                desc: STRING_TYPE,
+                createdOn: newDate(),
+                deadline: STRING_TYPE,
+              }))
+            && setTaskText(STRING_TYPE)
+          ) }
+        >
+          Listar!
+        </button>
+      </label>
+      <ListManagement
+        showDescription={ showDescription }
+        switchDescription={ setShowDescription }
+      />
+      {tasks
+        && <TaskList
+          tasks={ tasks }
+          showDescription={ showDescription }
+        />}
+    </main>
+  );
 }
