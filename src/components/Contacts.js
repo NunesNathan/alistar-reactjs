a47e11aa76357158b93734fef2a324d62702d43c
NunesNathan <nunesxd7@hotmail.com>
2022-03-11 02:21:27 -0300

    :recycle: (refactor) - Functional static components

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -1,39 +1,37 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-export default class Contacts extends Component {
-  render() {
-    return (
-      <ul className="nav justify-content-center border-bottom pb-1 mb-3">
-        <li className="nav-item text-center px-0">
-          <a
-            className="nav-link p-1 text-muted"
-            href="https://www.linkedin.com/in/nathannunes-/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Linkedin
-          </a>
-        </li>
-        <li className="nav-item text-center p-1">
-          <a
-            className="nav-link p-1 text-muted"
-            href="https://github.com/NunesNathan"
-            target="_blank"
-            rel="noreferrer"
-          >
-            GitHub
-          </a>
-        </li>
-        <li className="nav-item text-center p-1">
-          <a
-            className="nav-link p-1 text-muted"
-            href="https://nunesnathan.github.io/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Portifólio online
-          </a>
-        </li>
-      </ul>);
-  }
+export default function Contacts() {
+  return (
+    <ul className="nav justify-content-center border-bottom pb-1 mb-3">
+      <li className="nav-item text-center px-0">
+        <a
+          className="nav-link p-1 text-muted"
+          href="https://www.linkedin.com/in/nathannunes-/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Linkedin
+        </a>
+      </li>
+      <li className="nav-item text-center p-1">
+        <a
+          className="nav-link p-1 text-muted"
+          href="https://github.com/NunesNathan"
+          target="_blank"
+          rel="noreferrer"
+        >
+          GitHub
+        </a>
+      </li>
+      <li className="nav-item text-center p-1">
+        <a
+          className="nav-link p-1 text-muted"
+          href="https://nunesnathan.github.io/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Portifólio online
+        </a>
+      </li>
+    </ul>);
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,12 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-export default class Header extends Component {
-  render() {
-    return (
-      <header className="my-4 border-bottom">
-        <div className="container d-flex text-center flex-column">
-          <p className="display-3">À listar</p>
-          <p className="lead">O seu simples organizador de tarefas!</p>
-        </div>
-      </header>
-    );
-  }
+export default function Header() {
+  return (
+    <header className="my-4 border-bottom">
+      <div className="container d-flex text-center flex-column">
+        <p className="display-3">À listar</p>
+        <p className="lead">O seu simples organizador de tarefas!</p>
+      </div>
+    </header>
+  );
 }
