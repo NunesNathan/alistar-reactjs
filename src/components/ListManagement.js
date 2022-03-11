2738f386152666e685e2670a53a79ce7ea9cb730
NunesNathan <nunesxd7@hotmail.com>
2022-03-10 22:34:03 -0300

    :recycle: (refactor) - ListManagement functional

--- src/components/ListManagement.js
+++ src/components/ListManagement.js
@@ -1,62 +1,44 @@
-import React, { Component } from 'react';
-import PropType from 'prop-types';
+import React, { useContext, useEffect, useState } from 'react';
 import { Button } from 'react-bootstrap';
-import { removeClasses } from '../helpers/event';
+import TaskContext from '../context/TasksContext';
 import ClearListModal from './ClearListModal';
+import { removeClasses } from '../helpers/event';
 
-export default class ListManagement extends Component {
-  constructor() {
-    super();
+export default function ListManagement() {
+  const { showDescription, setShowDescription } = useContext(TaskContext);
+  const [clearConfirmation, setClearConfirmation] = useState(false);
 
-    this.state = { toClear: false };
-  }
+  const showClearListModal = () => setClearConfirmation(!clearConfirmation);
 
-  componentDidMount() {
-    removeClasses();
-  }
+  useEffect(removeClasses, []);
 
-  showClearListModal = () => {
-    const { toClear } = this.state;
-
-    this.setState({
-      toClear: !toClear,
-    });
-  }
-
-  render() {
-    const { switchDescription, showDescription, reRender } = this.props;
-    const { toClear } = this.state;
-    return (
+  return (
+    <TaskContext.Provider
+      value={ {
+        showClearListModal,
+        clearConfirmation,
+      } }
+    >
       <div className="row d-flex mt-2 mb-4">
         <button
           className="col-6 btn-n-info"
           type="button"
-          onClick={ switchDescription }
+          onClick={ setShowDescription(!showDescription) }
         >
           {showDescription
             ? 'Esconder Detalhes'
-            : 'Exibir Detalhes' }
+            : 'Exibir Detalhes'}
         </button>
         <Button
           id="clearButton"
           className="col-6 btn-n-danger"
           type="button"
-          onClick={ this.showClearListModal }
+          onClick={ showClearListModal }
         >
           Limpar lista
         </Button>
-        <ClearListModal
-          reRender={ reRender }
-          showClearListModal={ this.showClearListModal }
-          toClear={ toClear }
-        />
+        <ClearListModal />
       </div>
-    );
-  }
+    </TaskContext.Provider>
+  );
 }
-
-ListManagement.propTypes = {
-  switchDescription: PropType.func.isRequired,
-  showDescription: PropType.bool.isRequired,
-  reRender: PropType.func.isRequired,
-};
