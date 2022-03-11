96c4164659b33d787e763d4450ffbbe4b01f18c1
NunesNathan <nunesxd7@hotmail.com>
2022-03-10 22:45:25 -0300

    :recycle: (refactor) - Functional Tasklist component

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,31 +1,19 @@
-import React, { Component } from 'react';
-import PropType from 'prop-types';
+import React from 'react';
 import TaskItem from './TaskItem';
 
-export default class TaskList extends Component {
-  render() {
-    const { tasks, reRender, showDescription } = this.props;
-    return (
-      <ol
-        className=" container
+export default function tasts() {
+  return (
+    <ol
+      className=" container
         justify-content-center mb-5"
-      >
-        {
-          tasks.map((eachTask) => (
-            <TaskItem
-              { ...eachTask }
-              key={ eachTask.uniqKey }
-              reRender={ reRender }
-              showDescription={ showDescription }
-            />))
-        }
-      </ol>
-    );
-  }
+    >
+      {
+        tasks.map((eachTask) => (
+          <TaskItem
+            { ...eachTask }
+            key={ eachTask.uniqKey }
+          />))
+      }
+    </ol>
+  );
 }
-
-TaskList.propTypes = {
-  tasks: PropType.arrayOf(PropType.object).isRequired,
-  reRender: PropType.func.isRequired,
-  showDescription: PropType.bool.isRequired,
-};
