c77812fcbaa4a85025083713c388df6faa704608
NunesNathan <nunesxd7@hotmail.com>
2022-03-11 01:57:12 -0300

    :recycle: (refactor) - Functional Description component

--- src/components/Description.js
+++ src/components/Description.js
@@ -1,37 +1,34 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropType from 'prop-types';
 
-export default class Description extends Component {
-  render() {
-    const { desc, sendDescription, getDescription } = this.props;
-    return (
-      <div className="my-2">
-        <label
-          className="d-flex
+export default function Description({ desc, getDescription, sendDescription }) {
+  return (
+    <div className="my-2">
+      <label
+        className="d-flex
            justify-content-center label-desc"
-          htmlFor="desc"
+        htmlFor="desc"
+      >
+        <p className="mt-2 align-self-center">
+          Detalhes da tarefa:
+        </p>
+        <textarea
+          id="desc"
+          placeholder="descrição..."
+          defaultValue={ desc }
+          onChange={ ({ target: { value } }) => getDescription(value) }
+          className="mx-1 form-control bg-grey-2"
+        />
+        <button
+          onClick={ sendDescription }
+          className="align-self-center btn-n-outline-success"
+          type="button"
         >
-          <p className="mt-2 align-self-center">
-            Detalhes da tarefa:
-          </p>
-          <textarea
-            id="desc"
-            placeholder="descrição..."
-            value={ desc }
-            onChange={ getDescription }
-            className="mx-1 form-control bg-grey-2"
-          />
-          <button
-            onClick={ sendDescription }
-            className="align-self-center btn-n-outline-success"
-            type="button"
-          >
-            <i className="fas fa-location-arrow" />
-          </button>
-        </label>
-      </div>
-    );
-  }
+          <i className="fas fa-location-arrow" />
+        </button>
+      </label>
+    </div>
+  );
 }
 
 Description.propTypes = {
