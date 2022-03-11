830d6edbdd100e2998ace7c1766c7371871addaf
NunesNathan <nunesxd7@hotmail.com>
2022-03-10 22:49:48 -0300

    :ambulance: (task list) - Fix undefined tasks

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import TaskContext from '../context/TasksContext';
 import TaskItem from './TaskItem';
 
-export default function tasts() {
+export default function TaskList() {
+  const { tasks } = useContext(TaskContext);
+
   return (
     <ol
       className=" container
